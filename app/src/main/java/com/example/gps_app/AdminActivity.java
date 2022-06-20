bc574278df301ea281ec9e785495ab9168be002c
Mateus Padilha <mateuspmota2010@gmail.com>
2022-06-19 21:38:15 -0300

    Implementing splash screen and login with firebase

---
+++ app/src/main/java/com/example/gps_app/AdminActivity.java
@@ -0,0 +1,35 @@
+package com.example.gps_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AdminActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    CardView cardRoutes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+        getSupportActionBar().hide();
+
+        cardRoutes = findViewById(R.id.cardRoutes);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        cardRoutes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), GPSActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/gps_app/SplashScreen.java
@@ -0,0 +1,46 @@
+package com.example.gps_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+    Animation topAnim, bottomAnim;
+    GifImageView gif;
+    TextView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        logo = findViewById(R.id.textView2);
+        gif = findViewById(R.id.gifImageView);
+
+        gif.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
